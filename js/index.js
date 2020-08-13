41de8eac6c13d7d79d0eafe3ba20b6b8a35bac2b
douqiu <1044834391@qq.com>
2020-08-13 15:01:04 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    var mySwiper = new Swiper('.swiper-container',{
+        speed:2500,
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+          waitForTransition: true,
+        },
+    });
+});
